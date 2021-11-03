5421d11225c893cab21ec05a90702b9c37ec195a
Roya Salei <41925879+roysea@users.noreply.github.com>
2021-11-02 19:04:42 -0700

    save PC in big-endian mode while incrementing PC

--- IM/im.vhd
+++ IM/im.vhd
@@ -5,33 +5,37 @@ ENTITY instructionMemory IS
 	PORT (
 	CLK: IN STD_LOGIC;
 	MEMR: IN STD_LOGIC;
-	PC : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-	instruction : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
+	ADDR : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+	DOUT : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 END instructionMemory;
 ARCHITECTURE Behavioral OF instructionMemory IS
-	TYPE RAM_16_x_32 IS ARRAY(0 TO 15) OF std_logic_vector(31 DOWNTO 0);
-	SIGNAL IM : RAM_16_x_32 := (
-	"10001101001010000000000000000000",-- lw t0 0x00000000(t1
-   	"10101101001010000000000000000000",-- sw t0 0x00000000(t1)
-   	"00000001001010100100000000100000",-- add t0 t1 t2
-   	"00000001001010100100000000100010",-- sub t0 t1 t2
-   	"00000000000010010100000010000000",-- sll t0 t1 0x00000002
-   	"00000000000010010100000010000010",-- srl t0 t1 0x00000002
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000",
-   	"0000000000000000"
+	VARIABLE index : integer := 0;
+	TYPE RAM_16_x_8 IS ARRAY(0 TO 31) OF std_logic_vector(7 DOWNTO 0);
+	SIGNAL IM : RAM_32_x_8 := (
+	"10001101","00101000","00000000","00000000",-- lw t0 0x00000000(t1)
+   	"10101101","00101000","00000000","00000000",-- sw t0 0x00000000(t1)
+   	"00000001","00101010","01000000","00100000",-- add t0 t1 t2
+   	"00000001","00101010","01000000","00100010",-- sub t0 t1 t2
+   	"00000000","00001001","01000000","10000000",-- sll t0 t1 0x00000002
+   	"00000000","00001001","01000000","10000010",-- srl t0 t1 0x00000002
+   	"00000000","00000000","00000000","00000000",
+   	"00000000","00000000","00000000","00000000"
 	);
 BEGIN
-	-- Note: 4194304 = 0x0040 0000
-	-- reset when address is 003FFFFC else if readAddress is 0040 0000 then reset also
-	instruction <= x"00000000" when PC = x"003FFFFC" else 
-		IM(( to_integer(unsigned(PC)) - 4194304) /4);
+PROCESS(clk)
+	IF rising_edge(clk) AND MEMR='1' THEN
+		FOR i in DOUT'range LOOP
+			IF ( i = 24)
+  				DOUT(i) <= IM( to_integer(unsigned(ADDR)));
+			ELSE IF ( i = 16) 
+				DOUT(i) <= IM( to_integer(unsigned(ADDR + 1)));
+			ELSE IF ( i = 8) 
+				DOUT(i) <= IM( to_integer(unsigned(ADDR + 2)));
+			ELSE IF ( i = 0) 
+				DOUT(i) <= IM( to_integer(unsigned(ADDR + 3)));
+			END IF;
+		END LOOP;
+END PROCESS;
+
 END Behavioral;
